99a63a2d043d7beeb848bf3aaca77e54cd4f4827
Sam Kikoma <kikomasamson@gmail.com>
2024-08-10 18:25:54 +0200

    Schema model

---
+++ models/blog.js
@@ -0,0 +1,26 @@
+// mongoose schema
+
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url);
+
+const blogSchema = new mongoose.Schema({
+    blogId : String,
+    title : String,
+    authorName : String
+})
+
+blogSchema.set("toJSON",{
+    transform : (document,returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+module.exports = Blog
