3ef512588e83f4434ec6c86e158c6f60bbafc490
Sam Kikoma <kikomasamson@gmail.com>
2024-08-10 18:25:10 +0200

    API connection

---
+++ app.js
@@ -0,0 +1,13 @@
+const config = require("./utils/config")
+const blogsRouter = require("./controllers/blogs")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/blogs", blogsRouter)
+
+module.exports = app
